7d87d47fa32fae0172c68f34a2dd062fc620064f
Facundo Quintana <facunquintana@gmail.com>
2023-04-29 20:58:53 -0300

    test auth

--- src/oruga/routes/OrugaRoutes.jsx
+++ src/oruga/routes/OrugaRoutes.jsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { HomePage } from "../pages/HomePage"
+import { ClientsPage } from "../pages/ClientsPage"
 
 export const OrugaRoutes = () => {
   return (
    <Routes>
     <Route path="/" element={ <HomePage/> }/>
+    <Route path="/clientes" element={ <ClientsPage/> }/>
     <Route path="/*" element={ <Navigate to="/" /> }/>
    </Routes>
   )
